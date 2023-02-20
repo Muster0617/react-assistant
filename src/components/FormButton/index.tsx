a707d0fd697047df3e3d31692a073843dd906fce
xieteng <xieteng@cityos.com>
2023-02-20 14:23:39 +0800

    feat: 新增表单按钮

---
+++ src/components/FormButton/index.tsx
@@ -0,0 +1,33 @@
+import { Button } from 'antd';
+import styles from './index.less';
+
+export default ({
+  buttonClick,
+  buttonName,
+  label,
+  labelStyle,
+  required,
+  buttonProps,
+}) => {
+  return (
+    <div className={styles.form_item}>
+      <div className={styles.form_item_label} style={{ ...labelStyle }}>
+        {required && (
+          <div
+            style={{
+              color: '#ff4d4f',
+              marginRight: 4,
+            }}
+          >
+            *
+          </div>
+        )}
+        {label}
+        <span style={{ marginLeft: 2, marginRight: 8 }}>:</span>
+      </div>
+      <Button onClick={buttonClick} {...buttonProps}>
+        {buttonName}
+      </Button>
+    </div>
+  );
+};
